227eae94f49dd7a18947137e0c6aae3db500f692
FabrizioCmp <fabriziocampari01@gmail.com>
2022-11-10 17:45:54 +0100

    Bonus,supreBonus, vittoria Gioco

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 const btnGenerate = document.querySelector(".btn_generate");
 const difficultyEl = document.getElementById("difficulty");
 const resultsEl = document.querySelector(".results");
+const numeroDiBombe = 2;
 let celleScoperte = [];
 let punteggio = 0;
+let endGame = false;
 
 
 // al click del bottone genera il campo richesto
@@ -12,7 +14,6 @@ btnGenerate.addEventListener("click", function () {
 })
 
 // quando scopro tutte le celle il gioco finisce
-
 
 
 
@@ -26,7 +27,7 @@ btnGenerate.addEventListener("click", function () {
 function generateCampo(numCelle) {
     const campoEl = document.querySelector(".campo");
     const cellePerRiga = Math.sqrt(numCelle);
-    let lost = false;
+    endGame = false;
 
     campoEl.innerHTML = "";
     campoEl.classList.add("campo_border");
@@ -34,7 +35,7 @@ function generateCampo(numCelle) {
     punteggio = 0;
 
     // generazione lista con posizione delle bombe
-    const bombsList = generateBombs(16);
+    const bombsList = generateBombs(numeroDiBombe);
     console.log(bombsList);
 
     // creazione delle singole celle
@@ -49,11 +50,12 @@ function generateCampo(numCelle) {
         newCell.dataset.numCella = i + 1;
 
 
+
         //Al click sulla cella: effetti grafici & controllo bomba
         newCell.addEventListener("click", function () {
-            if (!lost) {
+            if (!endGame) {
                 this.classList.toggle(`cell_selected`);
-                punteggio ++;
+                punteggio++;
                 const cellSelected = parseInt(this.dataset.numCella);
                 if (bombsList.includes(cellSelected)) {
                     lost = true;
@@ -68,11 +70,20 @@ function generateCampo(numCelle) {
                             resultsEl.classList.add("results_transition");
                         }
                     }
-                }else if (!celleScoperte.includes(cellSelected)){
-                   celleScoperte.push(cellSelected);
+                } else if (!celleScoperte.includes(cellSelected)) {
+                    celleScoperte.push(cellSelected);
                 }
             }
+            if (celleScoperte.length === (parseInt(difficultyEl.value) - numeroDiBombe)) {
 
+                celleScoperte = [];
+                endGame = true;  // così le celle non sono più selezionabili
+                resultsEl.innerHTML = `<h2> HAI VINTO! `;
+                resultsEl.classList.add("results_transition");
+                for (let i = 0; i < bombsList.length; i++) {
+                    document.querySelector(`[data-num-cella="${bombsList[i]}"]`).classList.add("bkg_bomb")
+                }
+            }            
         });
     }
 }
